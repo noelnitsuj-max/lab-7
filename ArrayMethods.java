8b7253673d1c10fe8cf88cb38f4f9baf0c161d70
noelnitsuj-max <noelnitsuj@gmail.com>
2025-11-10 19:01:29 -0800

    Add files via upload

--- ArrayMethods.java
+++ ArrayMethods.java
@@ -86,5 +86,91 @@ public class ArrayMethods
         }
         return index;
     }
+    /**
+     * This method accesses the instance field myArray
+     * @return myArray The instance field array.
+     */
+    public int[] getArray(){
+        return myArray;
+    }
+    /**
+     * This method creates a copy of myArray
+     * @return copy A copy of myArray.
+     */
+    public int[] copyArray(){
+        int[] copy = Arrays.copyOf(myArray, myArray.length);
+        return copy;
+    }
+    /**
+     * Returns the index of the rightmost value that is equal to the key.
+     * @return index the index of the value.
+     * @param key The value chosen.
+     */
+    public int findLast(int key){
+        int index = 0;
+        int amount = count();
+        for(int x = amount - 1; x >= 0; x --){
+            if(myArray[x] == key){
+                index = x;
+                return index;
+            }
+        }
+        index = -1;
+        return index;
+    }
+     /**
+     * Returns the index of all values that equal the key.
+     * @return indexes An array with all the indexes of the values equal to the key.
+     * @param key The value chosen.
+     */
+    public int[] findAll(int key){
+        int amount = count();
+        int count = 0;
+        for(int x = 0; x <amount; x ++){
+            if(myArray[x] == key){
+                count ++;
+            }
+        }
+        int[] indexes = new int[count];
+        count = 0;
+        for(int x = 0; x <amount; x ++){
+            if(myArray[x] == key){
+                indexes[count] = x;
+                count ++;
+            }
+        }
+        return indexes;
+    }
+    /**
+     * This method returns an array with reversed values from the inputted array.
+     * @return retArray The reversed array.
+     * @param inArray The array that is being changed.
+     */
+    public int[] reverseArray(int[] inArray){
+        int index = 0;
+        int[] retArray = new int[inArray.length];
+        int amount = inArray.length;
+        for(int x = 0; x < amount; x ++){
+            retArray[x] = inArray[amount - x - 1];
+        }
+        return retArray;
+    }
+    /**
+    prints an int array, nicely formatted
+    @param inArray int array to print
+    */
+    public void print(int[ ] inArray)
+    {
+    System.out.print("{");
+    int i;
+    // print elements before the last, separated by commas
+    for (i = 0; i < inArray.length - 1; i++)
+    System.out.print(inArray[i] + ", ");
+    // print last element. Careful here to handle length 0
+    if (inArray.length > 0)
+    System.out.print(inArray[i]);
+    System.out.println("}");
+}
+    
     
 }
--- ArrayMethodsDemo.java
+++ ArrayMethodsDemo.java
@@ -22,6 +22,27 @@ test.findMax());
 System.out.println("The index of the largest number in myArray[] is: " +
 test.findIndexOfMax());
 //Part 2
-
+System.out.println("\n\nLab7 Pt2 - Let's continue to rumble :-)");
+System.out.println("=======================================");
+System.out.println("The last (or right most) index of 8 is: " +
+test.findLast(8));
+System.out.println("The last (or right most) index of 2 is: " +
+test.findLast(2));
+int[] findAll8s = test.findAll(8);
+System.out.print("8s were located in the following indices ");
+test.print(findAll8s);
+int[] myArray = test.copyArray();
+System.out.print("myArray[] is: ");
+test.print(myArray);
+int[] copyOfMyArray = test.copyArray();
+System.out.print("copyOfMyArray[] is: ");
+test.print(copyOfMyArray);
+System.out.println("myArray[] and copyOfMyArray[] are equal: " +
+Arrays.equals(test.getArray(),copyOfMyArray));
+copyOfMyArray = test.reverseArray(copyOfMyArray);
+System.out.print("copyOfMyArray[] reversed is: ");
+test.print(copyOfMyArray);
+System.out.println("myArray[] and copyOfMyArray[] are equal: " +
+Arrays.equals(test.getArray(),copyOfMyArray));
 }
 }
